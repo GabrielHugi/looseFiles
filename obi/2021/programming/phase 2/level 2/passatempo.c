68a9d9181899c6984b6f334203f76fdf14da1b15
Gabriel Hugi <gabrielhugi2@gmail.com>
2025-07-24 10:22:49 -0300

    just to save

--- obi/2021/programming/phase 2/level 2/passatempo.c
+++ obi/2021/programming/phase 2/level 2/passatempo.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define flush int c; while ((c = getchar()) != '\n' && c != EOF);
 
 //bro this one is actually hard so imma do this later
 
@@ -6,7 +8,27 @@ int main () {
   int x, y;
   scanf("%d", &x);
   scanf("%d", &y);
-  char board[x][y][2];
-  int valuesOnDaSide[x+y];
+  flush
+  char (*board)[y][3] = malloc(sizeof(char[x][y][3]));
+  int *valuesOnDaSide = malloc((x + y) * sizeof(int));
+  for (int i = 0; i <= y; i++) {
+    for (int i2 = 0; i2 <= x; i++) {
+      if (i == y) {
+        if (i2 != x) {
+          scanf("%d", &valuesOnDaSide[i2+i]);
+          flush
+        }
+      }
+      else if (i2 == x) {
+        scanf("%d", &valuesOnDaSide[i]);
+        flush
+      }
+      else {
+        scanf("%2s", board[i2][i]);
+      }
+    }
+  }
+  free(board);
+  free(valuesOnDaSide);
 
 }
